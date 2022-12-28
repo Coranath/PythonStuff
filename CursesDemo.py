2f169aa4387b2340ebfa93b064da3f44a8802a83
Levi <levi@mail.com>
2022-12-28 14:34:22 -0700

    Overlay text

--- CursesDemo.py
+++ CursesDemo.py
@@ -5,7 +5,8 @@ from curses import wrapper
 
 def main(stdscr):
     stdscr.clear()
-    stdscr.addstr('hello world!')
+    stdscr.addstr(0,1, 'hello world!', curses.A_STANDOUT)
+    stdscr.addstr(0,2, 'goodby world!')
     stdscr.getch()
 
 wrapper(main)
